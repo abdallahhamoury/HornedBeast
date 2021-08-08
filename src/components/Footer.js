c31364ea976809ef3550e23e347ead655e729cab
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-08-09 01:08:33 +0300

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    Abdallah Hamoury &#169;
+                </p>
+            </div>
+        )
+    }
+}
+
+
+export default Footer;
